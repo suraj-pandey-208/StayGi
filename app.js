72dd5c72a0a0e98d4fb45f72f2a004593b9de16a
Suraj Pandey <surajpandey0289@gmail.com>
2026-08-06 19:09:05 +0530

    env change

--- app.js
+++ app.js
@@ -121,7 +121,6 @@ const passport = require("passport")
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js")
 
-/* -------------------- SETUP -------------------- */
 const session = require("express-session");
 const MongoStore=require("connect-mongo").default
 const flash = require("connect-flash");
